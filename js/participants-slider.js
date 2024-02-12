f5e02e9fc425bb2e5903c845c727cc08be7a9bfd
Vlad <vladgod@mail.ru>
2024-02-12 17:16:27 +0300

    Добавляет первую версию слайдера участников для десктопа

--- js/participants-slider.js
+++ js/participants-slider.js
@@ -6,14 +6,15 @@ const sliderImages = document.querySelector('.participant__box');
 const participantPrevButton = document.querySelector('.participant__prev-button');
 //Находим кнопку вперёд
 const participantNextButton = document.querySelector('.participant__next-button');
-//Создаём массивоподобную коллекцию классов точек индикатора
-const participantDots = document.querySelectorAll('.participant__dot');
-//Создаём массив из найденных в контейнере слайдера изображений
+//Создаём массивоподобную коллекцию изображений участников
 const participantSlides = sliderImages.querySelectorAll('img');
-//Определяем количество изображений в массиве
-const participantSlideCount = participantSlides.length;
 //Определяем интервал автоматического перелистывания слайдов
-const interval = 4000;
+const interval = 4000000;
+//Определяем количество изображений в массиве
+let participantSlideCount = participantSlides.length;
+if (window.screen.width >= 1366) {
+  participantSlideCount = participantSlides.length/3;
+}
 //Создаём индекс активного слайда
 let paticipantSlideIndex = 0;
 //Создаём индекс направления перемещения (1-вперёд, 0-назад)
@@ -72,6 +73,19 @@ const paticipantsNextSlide = () => {
 //При загрузке окна сайта запускаем функцию слайдера
 window.addEventListener('load', () => {
   paticipantsSlide();
+});
+
+//При увеличении размера вьюпорта до 1366px и выше переставляем слайдер в начальное положение
+window.addEventListener('resize', () => {
+  if (window.matchMedia('(min-width: 1366px)').matches) {
+    paticipantSlideIndex = 0;
+    paticipantChangeSlide(0);
+    participantSlideCount = participantSlides.length/3;
+    document.querySelector('.participant__count').textContent = participantSlideCount;
+  } else {
+      participantSlideCount = participantSlides.length;
+      document.querySelector('.participant__count').textContent = participantSlideCount;
+  }
 });
 
 //Выводим в разметку количество слайдов в слайдере
